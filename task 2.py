f55a605534ea09ad901c146bcafa34f100f98db6
shaziabubacker <shaziabubackerjr@gmail.com>
2025-06-18 22:46:26 +0300

    Question 2

--- task 2.py
+++ task 2.py
@@ -1,16 +1,20 @@
 from openpyxl import load_workbook
 
-wb = load_workbook('sagatave_eksamenam.xlsx')
+wb = load_workbook('sagatave_eksamenam (1).xlsx')
 ws = wb['Lapa_0']
 max_row = ws.max_row
 
 count = 0
 
 for row in range(2, max_row + 1):
-    priority = ws['H' + str(row)].value
-    delivery_date = ws['J' + str(row)].value
+    priority = ws['H' + str(row)].value       # Prioritāte
+    delivery_date = ws['J' + str(row)].value  # Piegādes datums
 
-    if priority == 'High' and hasattr(delivery_date, 'year') and delivery_date.year == 2015:
-        count += 1
+    try:
+        if priority == 'High' and delivery_date.year == 2015:
+            count += 1
+    except:
+        # If delivery_date is not a date (e.g. None or string), skip it
+        continue
 
 print(count)
