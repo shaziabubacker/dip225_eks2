498b1e5b67cf3b08af3a99044d173520033bf81f
shaziabubacker <shaziabubackerjr@gmail.com>
2025-06-18 22:02:24 +0300

    Question 1

---
+++ task 1.py
@@ -0,0 +1,16 @@
+from openpyxl import load_workbook
+
+wb = load_workbook('sagatave_eksamenam.xlsx')
+ws = wb['Lapa_0']
+max_row = ws.max_row
+
+count = 0
+
+for row in range(2, max_row + 1):
+    address = ws['D' + str(row)].value
+    number = ws['L' + str(row)].value
+
+    if isinstance(address, str) and address.startswith('Ain') and isinstance(number, (int, float)) and number < 40:
+        count += 1
+
+print(count)
